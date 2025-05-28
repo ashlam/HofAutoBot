b9dd9d094fd4e0617df91b1966d4a2fb3ece72aa
guwenguang <andyloveyouu@163.com>
2025-05-28 20:42:59 +0800

    快速拷贝

---
+++ tools/cp_default_account.sh
@@ -0,0 +1,2 @@
+cp ../HofAutoBot2/configs/server_01/account_config.json configs/server_01/account_config.json
+cp ../HofAutoBot2/configs/server_02/account_config.json configs/server_02/account_config.json
